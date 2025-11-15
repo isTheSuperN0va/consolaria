28aeaa41c7c1e5eb0e8a842614d9e0230a0ff4b1
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2025-11-15 02:38:42 -0300

    + menu seletor basico

--- Menu.cs
+++ Menu.cs
@@ -1,13 +1,23 @@
 
 
-using System.ComponentModel;
+using System;
 
 public abstract class MenuBase
 {
+    public enum MenuTypes
+        {
+            Button,
+            Selector,
+            Keybind
+        }   
+
     public string label = "";
     public Action? action;
 
+    public MenuTypes menuType;
+
     public bool isSelected = false;
+    public abstract void Draw(bool isSelected);
 
 }
 
@@ -15,11 +25,12 @@ public class MenuButton : MenuBase
 {
     public MenuButton(string label, Action? action)
     {
+        this.menuType = MenuTypes.Button;
         this.label = label;
         this.action = action;
     }
 
-    public void Draw(bool isSelected)
+    override public void Draw(bool isSelected)
     {
         Console.BackgroundColor = isSelected ? Color.selectedColor : Color.backgroundColor;
         Console.WriteLine(label);
@@ -28,24 +39,37 @@ public class MenuButton : MenuBase
 
 public class MenuSelector : MenuBase
 {
-    public MenuSelector(string label, Action? action)
+    public byte index = 0;
+    string[] selector;
+    public MenuSelector(string label, Action? action, string[] selector)
     {
+        this.menuType = MenuTypes.Selector;
+        
         this.label = label;
         this.action = action;
+        this.selector = selector;
+    }
+    override public void Draw(bool isSelected)
+    {
+        index = (byte)((index + selector.Length) % selector.Length);
+        
+        Console.BackgroundColor = isSelected ? Color.selectedColor : Color.backgroundColor;
+        Console.WriteLine(label + " - " + selector[index]);
+
     }
 
-    public void Draw(string[] selector, int i)
+    public void Move(bool left)
     {
-        Console.BackgroundColor = isSelected ? Color.selectedColor : Color.backgroundColor;
-        Console.WriteLine(label + " - " + selector[i]);
-    }
+        if (left) { index++; }
+        else { index--; }
+    }   
 }
 
 static public class Menu
 {
-    static MenuButton[][] menu = {
-        new MenuButton[] {new MenuButton("Jogar", Nothing), new MenuButton("Opções", () => ChangeMenu(1)), new MenuButton("Sair", Nothing)},
-        new MenuButton[] {new MenuButton("Linguagem", Nothing), new MenuButton("Teclas", Nothing), new MenuButton("Cores", Nothing), new MenuButton("Voltar", Nothing)}};
+    static MenuBase[][] menu = {
+        new MenuBase[] {new MenuButton("Jogar", Nothing), new MenuButton("Opções", () => ChangeMenu(1)), new MenuButton("Sair", Nothing)},
+        new MenuBase[] {new MenuSelector("Linguagem", Nothing, new string[]{"PT-BR", "EN-US"}), new MenuButton("Teclas", Nothing), new MenuButton("Cores", Nothing), new MenuButton("Voltar", Nothing)}};
 
     static int selectedOption = 0;
     static int currentMenu = 0;
@@ -61,18 +85,17 @@ static public class Menu
             Console.BackgroundColor = Color.backgroundColor;
             Console.Clear();
         }
-    }
-    static public void Draw()
+    static void Draw()
     {
         bool isSelected = false;
 
-        for (int i = 0; i < menu.Length; i++)
+        for (int i = 0; i < menu[currentMenu].Length; i++)
         {
             isSelected = i == selectedOption;
             menu[currentMenu][i].Draw(isSelected);
         }
     }
-
+    }
     static bool HandleInput()
     {
         ConsoleKey key = Console.ReadKey(true).Key;
@@ -80,13 +103,17 @@ static public class Menu
         if (key == Keybinds.MenuUp) { selectedOption--; }
         else if (key == Keybinds.MenuDown) { selectedOption++; }
         else if (key == Keybinds.Confirm) { menu[currentMenu][selectedOption].action?.Invoke(); }
+        else if (key == Keybinds.MenuLeft && menu[currentMenu][selectedOption] is MenuSelector selLeft) { selLeft.Move(true); }
+        else if (key == Keybinds.MenuRight && menu[currentMenu][selectedOption] is MenuSelector selRight) { selRight.Move(false); }
         else if (key == Keybinds.Exit) { return false; }
 
-        selectedOption = (selectedOption + menu.Length) % menu.Length;
+        selectedOption = (selectedOption + menu[currentMenu].Length) % menu[currentMenu].Length;
         return true;
     }
+    
 
     static void ChangeMenu(int menu) { currentMenu = menu; }
 
-    static void Nothing() {  }   
+    static void Nothing() {  }
+
 }
--- Options.cs
+++ Options.cs
@@ -12,6 +12,10 @@ static public class Keybinds
     public static ConsoleKey MenuUpAlt = ConsoleKey.W;
     public static ConsoleKey MenuDown = ConsoleKey.DownArrow;
     public static ConsoleKey MenuDownAlt = ConsoleKey.S;
+    public static ConsoleKey MenuLeft = ConsoleKey.LeftArrow;
+    public static ConsoleKey MenuLeftAlt = ConsoleKey.A;
+    public static ConsoleKey MenuRight = ConsoleKey.RightArrow;
+    public static ConsoleKey MenuRightAlt = ConsoleKey.D;
     public static ConsoleKey Confirm = ConsoleKey.Enter;
     public static ConsoleKey ConfirmAlt = ConsoleKey.Spacebar;
     public static ConsoleKey Exit = ConsoleKey.Escape;
